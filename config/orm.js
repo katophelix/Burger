ecf5469b8d0d3b98a98279f4062509968e8370b2
katophelix <katharrismail@gmail.com>
2018-03-06 21:54:45 -0500

    initial pish

---
+++ config/orm.js
@@ -0,0 +1,32 @@
+
+var connection = require('./connection.js');
+
+
+var orm = {
+  selectAll: function (callback) {
+    connection.query('SELECT * FROM burgers', function (err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+  },
+
+  insertOne: function (burger_name, callback) {
+    connection.query('INSERT INTO burgers SET ?', {
+      burger_name: burger_name,
+      // devour: false,
+    }, function (err, result) {
+      if (err) throw err;
+      callback(result);
+    });
+  },
+
+  updateOne: function (burgerID, callback) {
+    connection.query('UPDATE burgers SET ? WHERE ?',
+      [{ devour: true }, { id: burgerID }], function (err, result) {
+        if (err) throw err;
+        callback(result);
+      });
+  }
+};
+// Export the ORM object in module.exports.
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Turkey');
+INSERT INTO burgers (burger_name) VALUES ('Double');
+INSERT INTO burgers (burger_name) VALUES ('Chili');
+INSERT INTO burgers (burger_name) VALUES ('Avacado');
+INSERT INTO burgers (burger_name) VALUES ('Swiss');
